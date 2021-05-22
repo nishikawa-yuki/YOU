c7e791cf4cc7f55ccaf2b19926ca536f3b6c9bff
西川勇気 <sussy168168@gmail.com>
2021-05-22 13:30:23 +0900

    homeのhtml,css.js変更

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,24 @@ $(function () {
   $(".openbtn").click(function () {
     $(this).toggleClass('active');
   });
+
+
+
+  $('.company').hide();  
+  $(window).ready(function() {
+    let logo = $('.company').offset().top;
+    let wh = $(window).height();
+    if (logo <= $(window).scrollTop() + wh) {
+      $('.company').fadeIn(5000);
+    }
+  });
+
+  $('.hamburger-button').hide();  
+  $(window).ready(function() {
+    let logo = $('.hamburger-button').offset().top;
+    let wh = $(window).height();
+    if (logo <= $(window).scrollTop() + wh) {
+      $('.hamburger-button').fadeIn(5000);
+    }
+  });
 });
